fe1c229af8759f5c1b864353c3a46f6ce3f3fd17
bga <bga.email@gmail.com>
2021-11-12 20:56:47 +0300

    + help output + `jdupes` extra args

--- find-unique-files.sh
+++ find-unique-files.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/bash
 set -eu -o pipefail
 
-DIR1=$1
-DIR2=$2
+help="cmd jdupesExtraArgs dir1 dir2"
+
+if [ "$#" -lt 2 ]; then
+	echo $help 2>&1
+	exit 1
+fi
+
+DIR1=${@: -2 : 1}
+DIR2=${@: -1}
+jdupesExtraArgs=${@: 1 : (( $# - 2 ))}
 
 comm -23 \
 	<(find "$DIR1" "$DIR2" -type f | sort) \
-	<(jdupes -r -T -T "$DIR1" "$DIR2"  | xargs -d $'\n' cygpath -u {}\; | awk -v p1="$DIR1" -v p2="$DIR2" '$0 ~ p1 && $0 ~ p2'  RS="\n\n" ORS="\n\n" | sort)
+	<(jdupes -r "$jdupesExtraArgs" "$DIR1" "$DIR2"  | xargs -d $'\n' cygpath -u {}\; | awk -v p1="$DIR1" -v p2="$DIR2" '$0 ~ p1 && $0 ~ p2'  RS="\n\n" ORS="\n\n" | sort)
