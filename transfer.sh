d190c37be1b1700a982823ed7379b1f41b15d006
bga <bga.email@gmail.com>
2022-05-03 13:51:15 +0300

    do not force copy to clipboard

--- transfer.sh
+++ transfer.sh
@@ -10,5 +10,5 @@ curl   -H "Connection: keep-alive" \
 	-H "Accept-Encoding: identity" \
 	-H "Accept-Language: en-US" \
 	-H "User-Agent: Mozilla/5.0"  \
-	--upload-file "${fileToUpload}" "https://transfer.sh/${fileName}" "$@" | (xclip 2> NUL || clip)
+	--upload-file "${fileToUpload}" "https://transfer.sh/${fileName}" "$@"
 # read -p "..."
