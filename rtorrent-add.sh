e65629942d0c9ab03d1d9c194d7f1a3ec3571b9f
Bga <bga.email@gmail.com>
2023-08-16 14:25:47 +0300

    + change shebang to bash + accept many input files

--- rtorrent-add.sh
+++ rtorrent-add.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/env bash
 set -eu 
 set -o pipefail || true
 
-scp "$@" rtorrent-add@s0.lan:~/
+while [ $# -gt 0 ]; do
+	file=$1
+	shift
+
+	chmod g+r "$file"
+	scp -B -p -O -F ~/bin/emptyfile -P 37422 -i ~/.ssh/s0.lan-rtorrent_add-id_ed25519 "$file" rtorrent-add@bga.ind.in:~/
+done
